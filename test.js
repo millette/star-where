a77364b037a952371f4d780686149e97ba5063c8
Robin Millette <robin@millette.info>
2019-09-05 16:41:58 -0400

    Longer test timeout.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from 'ava'
 import fn from './'
 
 test('title', async t => {
-  t.timeout(60000)
+  t.timeout(200000)
   const result = await fn('millette', 10)
   t.truthy(result.length > 10)
 })
